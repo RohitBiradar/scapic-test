e9a35893316888958503033e5cdfb1d7f5f97f08
rahul yadav <rahu5992@gmail.com>
2017-10-22 22:28:30 +0530

    added example files for hidden files.

---
+++ server/config/database.example.js
@@ -0,0 +1,26 @@
+{
+	"development": {
+		"username": "xxxxxxxxx",
+		"password": "xxxxxxx",
+		"database": "xxxxxxx",
+		"host": "localhost",
+		"dialect": "postgres",
+		"define": {
+			"underscored": false,
+			"freezeTableName": true
+		},
+		"logging": false
+	},
+	"production": {
+		"username": "xxxxxxxx",
+		"password": "xxxxxxxxx",
+		"database": "xxxxxxxxx",
+		"host": "127.0.0.1",
+		"dialect": "postgres",
+		"define": {
+			"underscored": false,
+			"freezeTableName": true
+		},
+		"logging": false
+	}
+}
---
+++ server/config/keys.example.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ *  @return DONT PUSH THIS FILE TO GIT - ADD IN IGNORE.
+ */
+
+module.exports = {
+	googleOAuth : {
+		clientID : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com',
+		clientSecret : 'xxxxxxxxxxxxxxx'
+	},
+	awsSecrets : {
+		'accessKeyId' : 'xxxxxxxxxxxxxxxxxxx',
+		'secretAccessKey' : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	},
+	awsRegions : {
+		dev : 'xxxxxx',
+		main : 'xxxxxxx'
+	}
+}
